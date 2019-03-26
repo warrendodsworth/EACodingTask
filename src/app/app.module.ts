632edff5c838ebee4ffaac164c646a307b37d873
Warren Dodsworth <warren@Warrens-MacBook-Pro.local>
2019-03-26 17:55:16 +1100

    ADD angular http & home page load and display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ContentLoaderModule } from '@netbasal/content-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+
+    ContentLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { of, throwError } from 'rxjs';
+import { delay, flatMap, map, retryWhen, tap } from 'rxjs/operators';
+
+import { CarShow } from '../../models/car-show';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  API = 'http://localhost:3000/api';
+  carShows: CarShow[];
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+    let count = 0;
+    const req = this.http.get<any>(`${this.API}/cars`);
+
+    const subscription = req
+      .pipe(
+        tap(() => console.log("HTTP request executed")),
+        map((res) => {
+          if (res == "") {
+            console.log('empty response', res);
+            throw count++;    //error will be picked up by retryWhen
+          } else
+            return res;
+        }),
+        retryWhen(errors => {
+          return errors.pipe(
+            delay(500),
+            flatMap(count => count == 3 ? throwError("giving up") : of(count)),
+            tap(() => console.log('retrying...'))
+          )
+        })
+      )
+      .subscribe(
+        res => {
+          this.carShows = res;
+          console.log("HTTP request success")
+        },
+        err => {
+          this.carShows = [];
+          subscription.unsubscribe();               // hide content loader animation
+          console.error('HTTP Error', err)
+        },
+        () => {
+          subscription.unsubscribe();
+          console.log('HTTP request completed.')
+        }
+      );
   }
+
 
 }
